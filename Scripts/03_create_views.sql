ec2127ae304ec02a36c5bda0629d4c55f3dc4c82
Stephen Tetley <stephen.tetley@gmail.com>
2026-01-13 22:25:35 +0000

    Removed ledger_balance from vw_all_accounts as it made no sense

--- Scripts/03_create_views.sql
+++ Scripts/03_create_views.sql
@@ -1,21 +1,29 @@
-
+LOAD Excel;
 
 CREATE OR REPLACE VIEW accounts_working.vw_all_transactions AS
-(SELECT 'Nationwide' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.nationwide)
+WITH cte AS (
+(SELECT 'Nationwide' AS source_account, * FROM accounts_working.nationwide)
 UNION ALL BY NAME
-(SELECT 'YBS Access Saver' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.ybs_access_saver)
+(SELECT 'YBS Access Saver' AS source_account, * FROM accounts_working.ybs_access_saver)
 UNION ALL BY NAME
-(SELECT 'YBS Closed Savings' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.ybs_closed_savings)
+(SELECT 'YBS Closed Savings' AS source_account, * FROM accounts_working.ybs_closed_savings)
 UNION ALL BY NAME
-(SELECT 'YBS Funeral Expenses' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.ybs_funeral_expenses)
+(SELECT 'YBS Funeral Expenses' AS source_account, * FROM accounts_working.ybs_funeral_expenses)
 UNION ALL BY NAME
-(SELECT 'YBS Triple Access Saver' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.ybs_triple_access_saver)
+(SELECT 'YBS Triple Access Saver' AS source_account, * FROM accounts_working.ybs_triple_access_saver)
 UNION ALL BY NAME
-(SELECT 'YBS Two Year Fixed-Rate ISA' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.ybs_two_year_frisa)
+(SELECT 'YBS Two Year Fixed-Rate ISA' AS source_account, * FROM accounts_working.ybs_two_year_frisa)
 UNION ALL BY NAME
-(SELECT 'Yorkshire Bank' AS source_account, * EXCLUDE (transaction_id) FROM accounts_working.yorkshire_bank)
+(SELECT 'Yorkshire Bank' AS source_account, * FROM accounts_working.yorkshire_bank)
+) 
+SELECT * EXCLUDE (transaction_id, ledger_balance) FROM cte
 ;
 
 
-SELECT * FROM accounts_working.vw_all_transactions
-ORDER BY transaction_date DESC;
+
+
+--CREATE OR REPLACE TEMPORARY TABLE temp_report AS 
+--SELECT * FROM accounts_working.vw_all_transactions ORDER BY transaction_date DESC, source_account ASC;
+--
+--COPY temp_report TO (getvariable('accounts_path') || 'all_transactions.xlsx') WITH (FORMAT xlsx, HEADER true);
+
